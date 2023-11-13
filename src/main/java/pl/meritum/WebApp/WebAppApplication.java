3e8d740e9d95cdc985380b6949f065cffba1893b
wiktordablju <wiktordablju@gmail.com>
2023-11-13 10:16:39 +0100

    13.11: Testowanie metod HTTP

--- src/main/java/pl/meritum/WebApp/WebAppApplication.java
+++ src/main/java/pl/meritum/WebApp/WebAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebAppApplication {
 
+	//1 klasa kontrolera, wszystkie metody http, response entity
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
 	}
--- src/main/java/pl/meritum/WebApp/controller/SampleController.java
+++ src/main/java/pl/meritum/WebApp/controller/SampleController.java
@@ -1,13 +1,43 @@
 package pl.meritum.WebApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/")
 public class SampleController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
+
+//    ResponseEntity odpowiada za latwiejsze przetwarzanie i zwracanie wartosci metod http, np wlasnie .ok czy .body
+    @GetMapping("/get")
+    public ResponseEntity<String> getTest() {
+//        .ok = status to 200 => nie ma bledu
+        return ResponseEntity.ok("TO JEST METODA GET");
     }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postTest(@RequestBody String resource) {
+//      httpstatus.created = kod http 201 czyli ze zostal utworzony pomyslnie
+//      .body to po prostu co ci zwraca
+        return ResponseEntity.status(HttpStatus.CREATED).body("Podesłałeś ten zasób POSTem: " + resource);
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<String> putTest(@RequestBody String resource) {
+        return ResponseEntity.ok("Podesłałeś ten zasób PUTem: " + resource);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteTest(@RequestBody String resource) {
+        return ResponseEntity.ok("Tutaj testuje DELETE: " + resource);
+    }
+
+    @PatchMapping("/patch")
+    public ResponseEntity<String> patchTest(@RequestBody String resource) {
+        return ResponseEntity.ok("Tutaj testuje PATCH: " + resource) ;
+    }
+
+
+
 }
